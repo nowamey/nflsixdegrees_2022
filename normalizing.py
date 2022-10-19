fdbcc1370d57bd6da7f49eb91218f08812f96ae6
nowamey <nowamey100@gmail.com>
2022-10-19 18:15:14 -0400

    early normalization idea

---
+++ normalizing.py
@@ -0,0 +1,5 @@
+# Idea, take the dataframe columns in question, take a dictionary counting up 1,2,3....n 
+# every time there is a *unique* college, give it an id for linking table
+#
+#Needs to be done for teams, positions,colleges,players...(sorta)
+#

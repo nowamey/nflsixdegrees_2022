411e6cd9b6f3e00077299ee2e4e4a66ebd85bb91
nowamey <nowamey100@gmail.com>
2022-11-03 20:06:50 -0400

    adjustments made for normalization

--- retrieval.py
+++ retrieval.py
@@ -11,6 +11,12 @@ comm = re.compile("<!--|-->")
 teams = ["buf","mia","nyj","nwe","oti","clt","jax","htx","rav","cin","cle","pit","kan","sdg","den","rai",
         "phi","dal","nyg","was","tam","atl","nor","car","min","gnb","det","chi","sfo","crd","ram","sea"]
 
+fullnames = ["Buffalo Bills","Miami Dolphins","New York Jets","New England Patriots","Tennessee Titans","Indianapolis Colts","Jacksonville Jaguars",
+             "Houston Texans","Baltimore Ravens","Cincinatti Bengals","Cleveland Browns","Pittsburgh Steelers","Kansas City Chiefs","Los Angeles Chargers",
+             "Denver Broncos","Las Vegas Raiders","Philadelphia Eagles","Dallas Cowboys","New York Giants","Washington Commanders","Tampa Bay Buccaneers",
+             "Atlanta Falcons","New Orleans Saints","Carolina Panthers","Minnesota Vikings","Green Bay Packers","Detroit Lions","Chicago Bears",
+             "San Francisco 49ers","Arizona Cardinals","Los Angeles Rams","Seattle Seahawks"]
+
 def gethtml(url):
     page = requests.get(url)
     soupdata = BeautifulSoup(comm.sub("",page.text),'lxml')
@@ -19,11 +25,17 @@ def getRoster(team):
     soup = gethtml(f"https://www.pro-football-reference.com/teams/{team}/2022_roster.htm")
     roster = soup.find('table',{'id':'roster'})
     return roster
-    
+
 if __name__=="__main__":
     #known column declarations for the roster pages
-    cols =["Number","Player","Age","Position","G","GS","Wt","Ht","College/Univ","Birthdate","yrs","Drafted"]
+    cols =["Number","Player","Age","Position","G","GS","Wt","Ht","College/Univ","Birthdate","yrs","Drafted","team"]
     data = pd.DataFrame(columns=cols)
+    teamcounter = 0
+    
+    #sets to help make linking tables for db
+    colleges = dict()
+    positions = dict()
+    teamnames = dict()
     
     for team in teams:
         roster = getRoster(team)
@@ -31,15 +43,68 @@ if __name__=="__main__":
         for row in roster.find_all('tr'):
             rowsdata = row.find_all('td')
             row = [i.text for i in rowsdata]
+            
+            #Taking into account undrafted players
+            row.append(fullnames[teamcounter])
+            
+                
             length = len(data)
-        
-            
-            if(len(row) ==12):
+            #print(row)
+            collegescount =1
+            positionscount = 1
+            teamnamescount = 1
+            if(len(row) == 13):
+
+                if row[7]!='' and row[7] not in colleges.values :
+                    colleges[row[7]] = collegescount
+                    collegescount+=1
+                if row[2]!='' and row[2] not in positions.values:
+                    positions[row[2]] = positionscount
+                    positionscount +=1
+                if row[12]!='' and row[12] not in teamnames.values:
+                    teamnames[row[12]] = teamnamescount
+                    teamnamescount+=1
+                if(row[11] == ""):
+                    row[11] = "Undrafted"
+                
+                #should populate the row with id's from the dictionary, rather than strings
+                if(row[7]!=''):row[7] = colleges[row[7]]
+                if(row[2]!=''):row[2] = positions[row[2]]
+                if(row[12]!=''):row[12] = teamnames[row[12]]
+                
                 data.loc[length] = row
-            
-            
-            
-    data.to_csv(r"C:\Users\nowam\sixdegrees\df.csv",index= False)
+                
+        teamcounter+=1
+    #Linking  tables set up for db
+    college_table = pd.DataFrame(columns=["college_id","college_name"])
+    college_counter=1
+    for college in colleges:
+        college_table.loc[len(college_table)] = [college_counter,college]
+        college_counter+=1
+    pos_counter =1
+    position_table = pd.DataFrame(columns= ["position_id","position"])
+    for position in positions:
+        position_table.loc[len(position_table)] = [pos_counter,position]
+        pos_counter+=1
+    team_table = pd.DataFrame(columns = ["team_id","team"])
+    team_counter=1
+    for team in teamnames:
+        team_table.loc[len(team_table)] = [team_counter,team]
+        team_counter+=1
+    
+    
+    
+    #college_table.to_csv(r"C:\Users\nowam\sixdegrees\colleges.csv",index= False)
+    #position_table.to_csv(r"C:\Users\nowam\sixdegrees\positions.csv",index= False)
+    #team_table.to_csv(r"C:\Users\nowam\sixdegrees\teams.csv",index= False)
+    
+    #taking primary player table/ swapping exact names with pos_id,college_id, and team_id
+    
+    data.to_csv(r"C:\Users\nowam\sixdegrees\rosters_data.csv",index= False)
+    
+    
+
+    
     
     
         

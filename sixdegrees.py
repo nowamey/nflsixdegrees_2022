49b7fe07a81bad12f150579b98dbfa96ab71cd85
nowamey <nowamey100@gmail.com>
2022-11-20 20:16:46 -0500

    adjustmentstodatatypes

--- sixdegrees.py
+++ sixdegrees.py
@@ -28,25 +28,27 @@ class SixDegrees:
     
     
     def __init__(self):
+        
         with open("players_dict.json","r") as f:
             self.players_dict = json.load(f)
         with open("teams_map.json","r") as f:
             self.teams_map = json.load(f)
         
-
-    def findpath(player_one,player_two):
-        """
-        """
+    
+    
     def run_menu(self):
         print("Welcome to Six Degrees of the NFL! Please enter Two NFL players you would like to connect!")
         time.sleep(.2)
         player1 = self.players_dict[input("Please enter first player name: ")]
         player2 = self.players_dict[input("Please enter second player name: ")]
-        return retrieval.Player.player
-
+        player1 = retrieval.Player(player1['player_name'],player1['position'],player1['teams_list'],player1['url'])
+        player2 = retrieval.Player(player2['player_name'],player2['position'],player2['teams_list'],player2['url'])
+        
+        return player1.findpath(player2)
+    
+    
         
 
 if __name__ =="__main__":
     sixdegrees= SixDegrees()
-    #print(sixdegrees.players_dict)
-    print(sixdegrees.players_dict["Stefon Diggs"])
+    sixdegrees.run_menu()

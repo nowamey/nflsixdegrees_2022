cfce119159f1189179301236d16e9f28630661e1
nowamey <nowamey100@gmail.com>
2022-12-20 13:12:52 -0500

    earlyjs, button/mainevent start

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+
+function findConnection(playerData,teamsData,player1,player2){
+    
+}
 async function mainEvent(){
     const playerData = await getData('/players_dict.json');
     const teamsData = await getData('/teams_map.json');
-
+    const form = document.querySelector('#searchForm');
+    const submit = document.querySelector('#startSearch');
+    console.log(form);
+    
+    form.addEventListener('submit', (submitEvent) =>{
+       submitEvent.preventDefault();
+       console.log('Yeehaw');
+    });
+   
     
 }
 async function getData(path){

c66952fa69feed4cc247e652983b9f108bc242e8
foshesss <foshessss@gmail.com>
2023-04-30 15:05:21 -0400

    yooo

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Graph = require("graphology")
 const { Sigma } = require("sigma")
 const data = require("./data.json")
 
-
 const container = document.getElementById("sigma-container")
 container.style.height = "100vh"
 container.style.width = "100vw"
@@ -14,3 +13,26 @@ const renderer = new Sigma(graph, container, {
     minEdgeSize: 0.2,
     maxEdgeSize: 0.5
 });
+
+
+renderer.setSetting("nodeReducer", (node, data) => {
+    const state = {}
+    const res = { ...data };
+  
+    if (state.hoveredNeighbors && !state.hoveredNeighbors.has(node) && state.hoveredNode !== node) {
+      res.label = "";
+      res.color = "#f6f6f6";
+    }
+  
+    if (state.selectedNode === node) {
+      res.highlighted = true;
+    } else if (state.suggestions && !state.suggestions.has(node)) {
+      res.label = "";
+      res.color = "#f6f6f6";
+    }
+
+    console.log(res);
+  
+    return res;
+  });
+

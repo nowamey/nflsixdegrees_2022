ed5027e5d01a650170bab1170320b1e988ca5d02
foshes <foshessss@gmail.com>
2023-05-06 05:34:21 -0400

    making it more interactive

--- webapp/index.js
+++ webapp/index.js
@@ -32,5 +32,50 @@ const renderer = sigma.init(container).drawingProperties({
   maxRatio: 20
 })
 
-renderer.parseJson("../data.json", renderer.draw)
+const render_direct_neighbors = event => {
+  // retrieve edges from graph's nodes
+  const node_edges = graph._nodes.get(event.content[0]).in
+  renderer.iterNodes(node => {
+    node.active = node.id === event.content[0]
+    // im too lazy to work out the logic for this, so i just negated the good nodes LOL
+    node.hidden = !(node_edges[node.id] !== undefined || node.id === event.content[0])
+  })
+  renderer.draw()
+}
+
+const remove_selection = () => {
+  renderer.iterNodes(node => {
+    node.active = false
+    node.hidden = false
+  })
+  renderer.draw()
+}
+
+renderer.parseJson("../data.json", () => {
+  console.log(renderer)
+  console.log(graph)
+  const clusters = {}
+  const id_clusters = {}
+  renderer.clusters = clusters
+  renderer.id_clusters = id_clusters
+  
+  renderer.iterNodes((node) => {
+    const {cluster} = node.attr
+    if (!clusters[cluster]) clusters[cluster] = []
+    clusters[cluster].push(node.id)
+    id_clusters[node.id] = cluster
+  })
+
+  // click on a node
+  renderer.bind("downnodes", render_direct_neighbors)
+  renderer.draw()
+})
+
+// used to untoggle a selection
+document.getElementById("remove-selection").addEventListener("click", remove_selection)
+
+
+
+
+
 
